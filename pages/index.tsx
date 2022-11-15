e47a6e354c1e5d09022f4ec3baee9997672e2075
ManasSarker <sarker01960@gmail.com>
2022-11-16 01:58:43 +0600

    created server site

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
-import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Header from "../components/Header";
+import { sanityClient, urlFor } from "../sanity";
+import { Post } from "../typings";
 
-const Home: NextPage = () => {
+interface Props {
+  posts: [Post];
+}
+
+export default function Home({ posts }: Props) {
+  console.log(posts);
   return (
     <div className=" max-w-7xl mx-auto">
       <Head>
@@ -35,6 +41,30 @@ const Home: NextPage = () => {
       </div>
     </div>
   );
-};
+}
 
-export default Home;
+export const getServerSideProps = async () => {
+  const query = `*[_type =='post']{
+    _id,
+    title,
+    slug,
+    author ->{
+    name,
+    image
+  },
+  description,
+  mainImage,
+  slug
+    
+  }
+  
+  `;
+
+  const posts = await sanityClient.fetch(query);
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
